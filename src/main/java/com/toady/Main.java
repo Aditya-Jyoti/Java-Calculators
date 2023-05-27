375731908caad40d4eacb49f13e484b4c1f2bae3
Aditya <rev.toady.py@gmail.com>
2023-05-27 17:47:18 +0530

    Finish calculator

--- src/main/java/com/toady/Main.java
+++ src/main/java/com/toady/Main.java
@@ -4,6 +4,6 @@ import com.toady.projects.Calculator;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println(Calculator.runCalculator());
     }
 }
--- src/main/java/com/toady/projects/Calculator.java
+++ src/main/java/com/toady/projects/Calculator.java
@@ -1,7 +1,68 @@
 package com.toady.projects;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Calculator {
-   public static int add(int a, int b) {
-    return a + b;
-   } 
+    public static String runCalculator() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter expression: ");
+        List<String> expression = new ArrayList<String>(Arrays.asList(scanner.nextLine().split(" ")));
+        scanner.close();
+
+        while (expression.size() != 1) {
+            if (expression.contains("/")) {
+                int opIdx = expression.indexOf("/");
+                double lhs = Double.parseDouble(expression.get(opIdx - 1));
+                double rhs = Double.parseDouble(expression.get(opIdx + 1));
+
+                expression.remove(opIdx + 1);
+                expression.remove(opIdx);
+                expression.remove(opIdx - 1);
+
+                expression.add(opIdx - 1, Double.toString(lhs / rhs));
+                continue;
+
+            } else if (expression.contains("*")) {
+                int opIdx = expression.indexOf("*");
+                double lhs = Double.parseDouble(expression.get(opIdx - 1));
+                double rhs = Double.parseDouble(expression.get(opIdx + 1));
+
+                expression.remove(opIdx + 1);
+                expression.remove(opIdx);
+                expression.remove(opIdx - 1);
+
+                expression.add(opIdx - 1, Double.toString(lhs * rhs));
+                continue;
+
+            } else if (expression.contains("+")) {
+                int opIdx = expression.indexOf("+");
+                double lhs = Double.parseDouble(expression.get(opIdx - 1));
+                double rhs = Double.parseDouble(expression.get(opIdx + 1));
+
+                expression.remove(opIdx + 1);
+                expression.remove(opIdx);
+                expression.remove(opIdx - 1);
+
+                expression.add(opIdx - 1, Double.toString(lhs + rhs));
+                continue;
+
+            } else if (expression.contains("-")) {
+                int opIdx = expression.indexOf("-");
+                double lhs = Double.parseDouble(expression.get(opIdx - 1));
+                double rhs = Double.parseDouble(expression.get(opIdx + 1));
+
+                expression.remove(opIdx + 1);
+                expression.remove(opIdx);
+                expression.remove(opIdx - 1);
+
+                expression.add(opIdx - 1, Double.toString(lhs - rhs));
+                continue;
+            }
+        }
+
+        return expression.get(0);
+    }
 }
