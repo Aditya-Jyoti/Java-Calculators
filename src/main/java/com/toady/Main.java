46058de3043a428eaf125e6bd9e522fb49eac5c4
Aditya <rev.toady.py@gmail.com>
2023-05-28 09:39:53 +0530

    Temp changes

--- src/main/java/com/toady/Main.java
+++ src/main/java/com/toady/Main.java
@@ -1,9 +1,26 @@
 package com.toady;
 
+import org.apache.commons.cli.*;
+import java.util.Arrays;
+
 import com.toady.projects.Calculator;
+import com.toady.projects.CalculatorGui;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(Calculator.runCalculator());
+    public static void main(String[] args) throws ParseException{
+        Options options = new Options();
+
+        Option projectToRun = new Option("p", "project", true, "project to run");
+        projectToRun.setRequired(true);
+        options.addOption(projectToRun);
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+        String project = cmd.getOptionValue("project");
+
+        if (Arrays.asList("calculator", "calc").contains(project)) {
+            Calculator.runCalculator();
+        }
+
     }
 }
---
+++ src/main/java/com/toady/projects/CalculatorGui.java
@@ -0,0 +1,9 @@
+package com.toady.projects;
+
+import javax.swing.JFrame;
+
+public class CalculatorGui {
+    public static void runCalculatorGui() {
+        JFrame mainFrame = new JFrame("Calculator");
+    }
+}
